e3805030e4894f41bf2f0c763ec4bc2e548d43dc
Ben Chatelain <ben@octop.ad>
2016-03-26 11:05:07 -0600

    Add new_user.sh script

---
+++ new_user.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -e
+
+#
+# new_user.sh
+# Creates a new unix user.
+#
+
+# User name is required
+if [[ $# -ne 1 ]]; then
+  echo "Usage: new_user.sh user_name"
+  exit 1
+fi
+user_name=$1
+
+# Secure password entry, stores in $password
+stty -echo
+read -p "Password: " password; echo
+stty echo
+
+echo "username: $user_name, password: $password"
+exit 0
+
+# Get a user ID value to be used when creating the new user
+user_max_id=$(dscl . -list /Users UniqueID | awk '{print $2}' | sort -ug | tail -1)
+user_number=$((user_max_id+1))
+
+echo "Creating a '$user_name' user with id = $user_number"
+dscl . -create /Users/$user_name
+dscl . -create /Users/$user_name UserShell /bin/bash
+dscl . -create /Users/$user_name RealName $user_name
+dscl . -create /Users/$user_name UniqueID $usernumber
+dscl . -create /Users/$user_name NFSHomeDirectory /Users/$user_name
+
+# Replace with a real password
+dscl . -passwd /Users/$user_name "$password"
+dscl . -create /Users/$user_name PrimaryGroupID $user_name
+
+# Create the user's home directory plus the .ssh directory
+mkdir -p /Users/$user_name/.ssh
+chmod 0700 /Users/$user_name/.ssh
+touch /Users/$user_name/.ssh/authorized_keys
+chmod 600 /Users/$user_name/.ssh/authorized_keys
